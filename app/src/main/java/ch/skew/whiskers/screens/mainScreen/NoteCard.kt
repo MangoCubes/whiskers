dc00bbb580c746f8fe6e3711f4bad8cd841564b5
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-08-06 23:31:34 +0900

    Notecard now displays date and basic content

--- app/src/main/java/ch/skew/whiskers/screens/mainScreen/NoteCard.kt
+++ app/src/main/java/ch/skew/whiskers/screens/mainScreen/NoteCard.kt
@@ -1,8 +1,9 @@
 package ch.skew.whiskers.screens.mainScreen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,14 +12,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import ch.skew.whiskers.misskey.data.Note
 import coil.compose.rememberAsyncImagePainter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteCard(
@@ -29,27 +35,35 @@ fun NoteCard(
         onClick = noteScreen,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Box(
-            Modifier
-                .padding(16.dp)
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             Row(
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
                     modifier = Modifier
-                        .size(48.dp)
+                        .size(30.dp)
                         .clip(CircleShape),
                     painter = rememberAsyncImagePainter(note.user.avatarUrl),
                     contentDescription = note.user.username
                 )
-                Column {
-                    note.user.name?.let {
-                        Text(it)
-                        Text("@" + note.user.username)
-                    } ?: Text("@" + note.user.username)
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        note.user.name?.let {
+                            "$it (@" + note.user.username + ")"
+                        } ?: ("@" + note.user.username),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    val createdAt = LocalDateTime.parse(note.createdAt, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    Text(createdAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 }
             }
+            note.text?.let { Text(it) }
         }
     }
 }
