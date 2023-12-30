ce948285abb2fbe0d4edc5d66aa55d76a6336d28
main <admin@skew.ch>
2023-12-31 01:02:55 +0900

    New data structure for reactions

--- app/src/main/java/ch/skew/whiskers/misskey/data/api/Reaction.kt
+++ app/src/main/java/ch/skew/whiskers/misskey/data/api/Reaction.kt
@@ -1,6 +1,28 @@
 package ch.skew.whiskers.misskey.data.api
 
+import ch.skew.whiskers.misskey.data.UserLite
 import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class GetReactionsReqData(
+    val noteId: String,
+    val type: String? = null,
+    val limit: Int? = 10,
+    val offset: Int? = 0,
+    val sinceId: String? = null,
+    val untilId: String? = null
+)
+
+@Serializable
+data class ReactionInfo(
+    val id: String,
+    val createdAt: String,
+    val user: UserLite,
+    val type: String
+)
+
+typealias GetReactionsResData = List<ReactionInfo>
 
 @Serializable
 data class CreateReactionReqData(
